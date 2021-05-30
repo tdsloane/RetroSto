71596503fbb62c45e621cce8732defcdff60d7fd
tdsloane <76671199+tdsloane@users.noreply.github.com>
2021-05-30 14:38:09 -0700

    MongoDB Migrated
    MongoDB was added as the database of choice within the application. Djongo was used to integrate MongoDB and Django.  Initial migration complete.

--- RetroSto/urls.py
+++ RetroSto/urls.py
@@ -18,8 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', admin.site.urls),
-    path('register/', admin.site.urls),
-    path('login/', admin.site.urls),
-    path('about/', admin.site.urls),
 ]
