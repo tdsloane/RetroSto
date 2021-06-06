a18174692bfffc1694637096d46529d021335277
tdsloane <76671199+tdsloane@users.noreply.github.com>
2021-06-06 05:28:46 -0400

    Final touches on user profiles

--- catolog/views.py
+++ catolog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def collection(request):
+    return render(request, 'catolog/collection.html', {'title':'Collection'})
+
+def search(request):
+    return render(request, 'catolog/search.html', {'title': 'Search'})
--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.home, name='about'),
-    
+    path('about/', views.about, name='about'),
 ]
