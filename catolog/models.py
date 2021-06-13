6d7617963c55353c7568d6e014cd6146a24bb8db
tdsloane <76671199+tdsloane@users.noreply.github.com>
2021-06-13 15:50:06 -0400

    run-able push

--- catolog/models.py
+++ catolog/models.py
@@ -1,18 +1,8 @@
 from django.db import models
-from .views import group_games
 # Create your models here.
 
 class Game(models.Model):
-    for game in group_games:
-        name = game.name
-# def save(self, *args, **kwargs):
-#     super(Profile, self).save(*args, **kwargs)
-    # name = models.CharField(max_length=50)
-    # cover = models.URLField()
-    # storyline = models.TextField(max_length=500)
-    # summary = models.TextField(max_length=500)
-    # genre_set = models.CharField(max_length=50)
-    name = group_games
-    
+    pass
+
 class UserSearch():
     user_input = str
