583f2701a5362556ede82605a988b3149e842cb7
tdsloane <76671199+tdsloane@users.noreply.github.com>
2021-06-13 17:07:06 -0400

    Models midpoint

--- catolog/models.py
+++ catolog/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+# from .views import group_games
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Game(models.Model):
     pass
+    # for game in group_games.values():
+    #     collector = models.ForeignKey(User, on_delete=models.CASCADE)
+    #     for i in game.values():
+    #         name = models.CharField(max_length=50)
+    #         cover = models.URLField()
+    #         storyline = models.TextField(max_length=500)
+    #         summary = models.TextField(max_length=500)
+    #         genre_set = models.CharField(max_length=50)
 
+        
+    
+# def save(self, *args, **kwargs):
+#     super(Profile, self).save(*args, **kwargs)    
 class UserSearch():
-    user_input = str
+    pass
