0e41d18ae8db9be9f34a17c4f80722e01311e288
tdsloane <76671199+tdsloane@users.noreply.github.com>
2021-06-13 15:47:14 -0400

    Cover.url Fix

--- catolog/models.py
+++ catolog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from .views import group_games
 # Create your models here.
 
 class Game(models.Model):
-    pass
-
+    for game in group_games:
+        name = game.name
+# def save(self, *args, **kwargs):
+#     super(Profile, self).save(*args, **kwargs)
+    # name = models.CharField(max_length=50)
+    # cover = models.URLField()
+    # storyline = models.TextField(max_length=500)
+    # summary = models.TextField(max_length=500)
+    # genre_set = models.CharField(max_length=50)
+    name = group_games
+    
 class UserSearch():
     user_input = str
